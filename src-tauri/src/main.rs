9625efe84bebb56ef9e8b257f567945f5fa914f2
Felix Wotschofsky <hello@felisk.io>
2022-12-25 22:57:41 +0100

    Use system_shutdown library 📚

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,66 +4,22 @@
    windows_subsystem = "windows"
 )]
 
-use std::process::Command;
-use std::env;
+use system_shutdown::shutdown as system_shutdown;
+use system_shutdown::sleep as system_sleep;
 
 #[tauri::command]
 fn sleep() -> () {
-   match env::consts::OS {
-      "linux" => {
-         Command::new("sh")
-                  .arg("-c")
-                  .arg("systemctl suspend")
-                  .output()
-                  .expect("failed to execute process");
-      },
-      "macos" => {
-         Command::new("sh")
-                  .arg("-c")
-                  .arg("pmset sleepnow")
-                  .output()
-                  .expect("failed to execute process");
-      },
-      "windows" => {
-         Command::new("cmd")
-                  .arg("-c")
-                  .arg("shutdown /h")
-                  .output()
-                  .expect("failed to execute process");
-      },
-      _ => {
-         println!("Unsupported OS");
-      }
+   match system_sleep() {
+      Ok(_) => println!("Shutting down, bye!"),
+      Err(error) => eprintln!("Failed to shut down: {}", error),
    }
 }
 
 #[tauri::command]
 fn shutdown() -> () {
-   match env::consts::OS {
-      "linux" => {
-         Command::new("sh")
-                  .arg("-c")
-                  .arg("shutdown now")
-                  .output()
-                  .expect("failed to execute process");
-      },
-      "macos" => {
-         Command::new("sh")
-                  .arg("-c")
-                  .arg("osascript -e 'tell app \"System Events\" to shut down'")
-                  .output()
-                  .expect("failed to execute process");
-      },
-      "windows" => {
-         Command::new("cmd")
-                  .arg("-c")
-                  .arg("shutdown /s")
-                  .output()
-                  .expect("failed to execute process");
-      },
-      _ => {
-         println!("Unsupported OS");
-      }
+   match system_shutdown() {
+      Ok(_) => println!("Shutting down, bye!"),
+      Err(error) => eprintln!("Failed to shut down: {}", error),
    }
 }
 
