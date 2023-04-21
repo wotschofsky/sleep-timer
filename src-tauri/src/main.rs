8825b87ce0b399821b81d03e2f0ad80086f93cb4
Felix Wotschofsky <hello@felisk.io>
2023-04-21 23:43:07 +0200

    Format Rust code ✨

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,7 +1,6 @@
-
 #![cfg_attr(
-   all(not(debug_assertions), target_os = "windows"),
-   windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
 use system_shutdown::shutdown as system_shutdown;
@@ -9,23 +8,23 @@ use system_shutdown::sleep as system_sleep;
 
 #[tauri::command]
 fn sleep() -> () {
-   match system_sleep() {
-      Ok(_) => println!("Shutting down, bye!"),
-      Err(error) => eprintln!("Failed to shut down: {}", error),
-   }
+    match system_sleep() {
+        Ok(_) => println!("Shutting down, bye!"),
+        Err(error) => eprintln!("Failed to shut down: {}", error),
+    }
 }
 
 #[tauri::command]
 fn shutdown() -> () {
-   match system_shutdown() {
-      Ok(_) => println!("Shutting down, bye!"),
-      Err(error) => eprintln!("Failed to shut down: {}", error),
-   }
+    match system_shutdown() {
+        Ok(_) => println!("Shutting down, bye!"),
+        Err(error) => eprintln!("Failed to shut down: {}", error),
+    }
 }
 
 fn main() {
-   tauri::Builder::default()
-      .invoke_handler(tauri::generate_handler![sleep, shutdown])
-      .run(tauri::generate_context!())
-      .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![sleep, shutdown])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
